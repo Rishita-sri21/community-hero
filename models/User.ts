3d268331ab8620cb26fe84b3eb6a256da3af2609
Rishita-sri21 <rishitasrivastava21@gmail.com>
2026-06-30 23:58:01 +0530

    fix

--- models/User.ts
+++ models/User.ts
@@ -3,32 +3,25 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
     name: String,
-
     email: {
       type: String,
       unique: true,
       required: true,
     },
-
     password: {
       type: String,
       required: true,
     },
-
     avatar: String,
-
     role: {
       type: String,
       default: "Volunteer",
     },
-
     points: {
       type: Number,
       default: 0,
     },
-
     district: String,
-
     joinedSquads: [String],
   },
   { timestamps: true }
